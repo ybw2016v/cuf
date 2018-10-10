16c4e167f0a399ee4230f6f6e4df7da3e6c6b1de
yu <ybw2016v@126.com>
2018-10-10 16:07:48 +0800

    删除若干无用文件

--- Makefile
+++
@@ -1,3 +0,0 @@
-all:culib3.so
-culib3.so:calkel3.cu
-	nvcc calkel3.cu -shared --compiler-options "-fPIC"  -o calkel3.so
--- usercode.c
+++ usercode.c
@@ -16,14 +16,14 @@ void usercode(float * p1,float * vx,float * vy,float * vz,float *z0,int xar, int
 
 */
 
-void usercode(float * p1,float * vx,float * vy,float * vz,float *z0,int xar,  int yar,int zar,float m,int i)
-{
-    p1[64*xar+64*yar+64*zar]=sin(0.1*i);
-    // return 0;
-}
-
-
-// void usercode(float * p1,float * vx1,float * vz1,float *z0j,int xar, int yar,float m,int i)
+// void usercode(float * p1,float * vx,float * vy,float * vz,float *z0,int xar,  int yar,int zar,float m,int i)
 // {
-//     p1[4*xar+4*yar]=sin(0.1*i);
+//     p1[64*xar+64*yar+64*zar]=sin(0.1*i);
+//     // return 0;
 // }
+
+
+void usercode(float * p1,float * vx1,float * vz1,float *z0j,int xar, int yar,float m,int i)
+{
+    p1[4*xar+4*yar]=sin(0.1*i);
+}
